6639a592255f5b946d006de3c122e985b8f06c85
Keiho Sakapon <git@saka-pon.net>
2015-02-22 23:15:49 +0900

    Add AppModel class

---
+++ PlanetClock/PlanetClock/AppModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KLibrary.Labs.Reactive;
+using KLibrary.Labs.Reactive.Models;
+
+namespace PlanetClock
+{
+    public class AppModel
+    {
+        public IObservable<DateTime> JustMinutesArrived { get; private set; }
+        public IObservableGetProperty<int> Hour { get; private set; }
+        public IObservableGetProperty<int> Minute { get; private set; }
+
+        public AppModel()
+        {
+            JustMinutesArrived = new PeriodicTimer2(TimeSpan.FromMinutes(1), GetNextJustTime);
+            Hour = JustMinutesArrived
+                .Filter(dt => dt.Month == 0)
+                .Map(dt => dt.Hour)
+                .ToGetProperty(DateTime.Now.Hour);
+            Minute = JustMinutesArrived
+                .Map(dt => dt.Minute)
+                .ToGetProperty(DateTime.Now.Minute);
+        }
+
+        static DateTime GetNextJustTime()
+        {
+            var n = DateTime.Now.AddMinutes(1);
+            return new DateTime(n.Year, n.Month, n.Day, n.Hour, n.Minute, 0);
+        }
+    }
+}
