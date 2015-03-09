3928c86917063b8396b914e1680a321448857656
Keiho Sakapon <github@saka-pon.net>
2015-03-09 19:26:18 +0900

    Refactor

--- PlanetClock/PlanetClock/AppModel.cs
+++ PlanetClock/PlanetClock/AppModel.cs
@@ -10,25 +10,21 @@ namespace PlanetClock
         const int TickMilliseconds = 20;
         static readonly TimeSpan TickInterval = TimeSpan.FromMilliseconds(TickMilliseconds);
 
-        public IObservable<DateTime> JustTicksArrived { get; private set; }
+        IObservable<DateTime> _JustTicksArrived;
         public IObservableGetProperty<DateTime> JustTicks { get; private set; }
 
         public AppModel()
         {
             var initialTime = DateTime.Now;
 
-            JustTicksArrived = new PeriodicTimer2(TickInterval, () => GetNextJustTicks(initialTime));
-            JustTicks = JustTicksArrived.ToGetProperty(GetJustTicks(initialTime));
+            _JustTicksArrived = new PeriodicTimer2(TickInterval, () => GetNextJustTicks(initialTime));
+            JustTicks = _JustTicksArrived.ToGetProperty(GetJustTicks(initialTime));
         }
 
-        static DateTime GetJustTicks(DateTime dt)
-        {
-            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, (dt.Millisecond / TickMilliseconds) * TickMilliseconds);
-        }
+        static readonly Func<DateTime, DateTime> GetJustTicks = dt =>
+            new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, (dt.Millisecond / TickMilliseconds) * TickMilliseconds);
 
-        static DateTime GetNextJustTicks(DateTime dt)
-        {
-            return GetJustTicks(dt).Add(TickInterval);
-        }
+        static readonly Func<DateTime, DateTime> GetNextJustTicks = dt =>
+            GetJustTicks(dt).Add(TickInterval);
     }
 }
