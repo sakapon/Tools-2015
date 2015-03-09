039e148a0e3bf0f51af5f46cb4fef0a0aa402d5a
Keiho Sakapon <github@saka-pon.net>
2015-03-09 19:19:01 +0900

    Update model

--- PlanetClock/PlanetClock/AppModel.cs
+++ PlanetClock/PlanetClock/AppModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using KLibrary.Labs.Reactive;
 using KLibrary.Labs.Reactive.Models;
 
 namespace PlanetClock
@@ -14,28 +13,12 @@ namespace PlanetClock
         public IObservable<DateTime> JustTicksArrived { get; private set; }
         public IObservableGetProperty<DateTime> JustTicks { get; private set; }
 
-        public IObservableGetProperty<int> Hour { get; private set; }
-        public IObservableGetProperty<int> Minute { get; private set; }
-
-        public IObservableGetProperty<double> HourInDouble { get; private set; }
-        public IObservableGetProperty<double> SecondInDouble { get; private set; }
-
         public AppModel()
         {
             var initialTime = DateTime.Now;
 
             JustTicksArrived = new PeriodicTimer2(TickInterval, () => GetNextJustTicks(initialTime));
             JustTicks = JustTicksArrived.ToGetProperty(GetJustTicks(initialTime));
-
-            Hour = JustTicks.ToGetProperty(dt => dt.Hour);
-            Minute = JustTicks.ToGetProperty(dt => dt.Minute);
-
-            HourInDouble = JustTicks
-                .Filter(dt => dt.Second == 0 && dt.Millisecond == 0)
-                .Map(ToHourInDouble)
-                .ToGetProperty(ToHourInDouble(initialTime));
-            SecondInDouble = JustTicks
-                .ToGetProperty(ToSecondInDouble);
         }
 
         static DateTime GetJustTicks(DateTime dt)
@@ -47,8 +30,5 @@ namespace PlanetClock
         {
             return GetJustTicks(dt).Add(TickInterval);
         }
-
-        static readonly Func<DateTime, double> ToHourInDouble = dt => dt.Hour + dt.Minute / 60.0;
-        static readonly Func<DateTime, double> ToSecondInDouble = dt => dt.Second + dt.Millisecond / 1000.0;
     }
 }
--- PlanetClock/PlanetClock/MainViewModel.cs
+++ PlanetClock/PlanetClock/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using KLibrary.Labs.Reactive;
 using KLibrary.Labs.Reactive.Models;
 
 namespace PlanetClock
@@ -12,13 +13,34 @@ namespace PlanetClock
         const double HourRadius = 100;
 
         public AppModel AppModel { get; private set; }
+
+        public IObservableGetProperty<int> Hour { get; private set; }
+        public IObservableGetProperty<int> Minute { get; private set; }
+
+        public IObservableGetProperty<double> HourInDouble { get; private set; }
+        public IObservableGetProperty<double> SecondInDouble { get; private set; }
+
         public IObservableGetProperty<Vector> HourTranslate { get; private set; }
 
         public MainViewModel()
         {
             AppModel = new AppModel();
-            HourTranslate = AppModel.HourInDouble.ToGetProperty(HourToTranslate);
+
+            Hour = AppModel.JustTicks.ToGetProperty(dt => dt.Hour);
+            Minute = AppModel.JustTicks.ToGetProperty(dt => dt.Minute);
+
+            HourInDouble = AppModel.JustTicks
+                .Filter(dt => dt.Second == 0 && dt.Millisecond == 0)
+                .Map(ToHourInDouble)
+                .ToGetProperty(ToHourInDouble(AppModel.JustTicks.Value));
+            SecondInDouble = AppModel.JustTicks
+                .ToGetProperty(ToSecondInDouble);
+
+            HourTranslate = HourInDouble.ToGetProperty(HourToTranslate);
         }
+
+        static readonly Func<DateTime, double> ToHourInDouble = dt => dt.Hour + dt.Minute / 60.0;
+        static readonly Func<DateTime, double> ToSecondInDouble = dt => dt.Second + dt.Millisecond / 1000.0;
 
         static Vector HourToTranslate(double hour)
         {
