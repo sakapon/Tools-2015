5b07a78f6fb5b06dcec877606145087c3d38a21d
Keiho Sakapon <git@saka-pon.net>
2015-04-21 19:09:14 +0900

    Add AppModel class

---
+++ VisionPlate/VisionPlate/AppModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using KLibrary.Labs.ObservableModel;
+
+namespace VisionPlate
+{
+    public class AppModel
+    {
+        ISettableProperty<WriteableBitmap> _VideoBitmap;
+        public IGetOnlyProperty<WriteableBitmap> VideoBitmap { get; private set; }
+
+        public AppModel()
+        {
+            _VideoBitmap = ObservableProperty.CreateSettable<WriteableBitmap>(null);
+            VideoBitmap = _VideoBitmap.ToGetOnlyMask();
+        }
+    }
+}
