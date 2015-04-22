620fa6105e698d58599e4166b902c1d6e8ca8f73
Keiho Sakapon <git@saka-pon.net>
2015-04-22 16:43:15 +0900

    Add classes

---
+++ VisionPlate/VisionPlate/DispatchableBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VisionPlate
+{
+    public abstract class DispatchableBase
+    {
+        public SynchronizationContext InitialContext { get; private set; }
+
+        public DispatchableBase()
+        {
+            InitialContext = SynchronizationContext.Current;
+        }
+
+        protected void InvokeOnInitialThread(Action action)
+        {
+            if (action == null) return;
+            if (InitialContext == null) return;
+
+            InitialContext.Send(o => action(), null);
+        }
+
+        protected void InvokeOnInitialThreadAsync(Action action)
+        {
+            if (action == null) return;
+            if (InitialContext == null) return;
+
+            InitialContext.Post(o => action(), null);
+        }
+    }
+}
---
+++ VisionPlate/VisionPlate/VideoInput.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using KLibrary.Labs.ObservableModel;
+
+namespace VisionPlate
+{
+    public class VideoInput : IDisposable
+    {
+        VideoCaptureDevice _device;
+
+        public IGetOnlyProperty<Bitmap> FrameArrived { get; private set; }
+
+        public VideoInput(string deviceMoniker)
+        {
+            _device = new VideoCaptureDevice(deviceMoniker);
+
+            FrameArrived = Observable.FromEventPattern<NewFrameEventArgs>(_device, "NewFrame")
+                .Select(_ => _.EventArgs.Frame)
+                .ToGetOnly(null, true);
+
+            // 指定された幅に最も近い VideoCapabilities。
+            _device.VideoResolution = _device.VideoCapabilities.OrderBy(c => Math.Abs(c.FrameSize.Width - 960)).FirstOrDefault();
+            _device.Start();
+        }
+
+        public void StopAsync()
+        {
+            if (_device.IsRunning) _device.SignalToStop();
+        }
+
+        ~VideoInput()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            try
+            {
+                StopAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
