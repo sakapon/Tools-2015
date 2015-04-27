cdff239186d0defe44ba6ba9a30cd5f921c18306
Keiho Sakapon <git@saka-pon.net>
2015-04-27 19:46:19 +0900

    Add classes

---
+++ DepthMeasure/DepthMeasure/AppModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using KLibrary.Labs.ObservableModel;
+using Microsoft.Kinect;
+
+namespace DepthMeasure
+{
+    public class AppModel
+    {
+        const double Frequency = 30;
+        static readonly TimeSpan FramesInterval = TimeSpan.FromSeconds(1 / Frequency);
+
+        public AppModel()
+        {
+            var kinect = new AsyncKinectManager();
+            kinect.SensorConnected
+                .Subscribe(sensor =>
+                {
+                    sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+                    try
+                    {
+                        sensor.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        // センサーが他のプロセスに既に使用されている場合に発生します。
+                        Debug.WriteLine(ex);
+                    }
+                });
+            kinect.SensorDisconnected
+                .Subscribe(sensor => sensor.Stop());
+            kinect.Initialize();
+
+            var frameData = Observable.Interval(FramesInterval)
+                .Select(_ => new
+                {
+                    ColorData = kinect.Sensor.Value.GetColorData(FramesInterval),
+                    DepthData = kinect.Sensor.Value.GetDepthData(FramesInterval),
+                })
+                .ToGetOnly(null);
+        }
+    }
+}
---
+++ DepthMeasure/DepthMeasure/KinectHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Media.Media3D;
+using Microsoft.Kinect;
+
+namespace DepthMeasure
+{
+    public static class KinectHelper
+    {
+        public static Vector3D ToVector3D(this SkeletonPoint p)
+        {
+            return new Vector3D(p.X, p.Y, p.Z);
+        }
+
+        public static SkeletonPoint ToSkeletonPoint(this Vector3D v)
+        {
+            return new SkeletonPoint { X = (float)v.X, Y = (float)v.Y, Z = (float)v.Z };
+        }
+
+        public static void EnableWithDefaultSmoothing(this SkeletonStream stream)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+
+            stream.Enable(new TransformSmoothParameters
+            {
+                Correction = 0.5f,
+                JitterRadius = 0.05f,
+                MaxDeviationRadius = 0.04f,
+                Prediction = 0.0f,
+                Smoothing = 0.5f,
+            });
+        }
+
+        public static Skeleton[] GetSkeletonData(this KinectSensor sensor, TimeSpan timeout)
+        {
+            if (sensor == null || !sensor.IsRunning) return null;
+
+            try
+            {
+                using (var frame = sensor.SkeletonStream.OpenNextFrame((int)timeout.TotalMilliseconds))
+                {
+                    if (frame == null) return null;
+
+                    var skeletonData = new Skeleton[frame.SkeletonArrayLength];
+                    frame.CopySkeletonDataTo(skeletonData);
+                    return skeletonData;
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                // センサーが稼働していない、またはストリームが有効になっていないときにフレームを取得すると発生します。
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+
+        public static byte[] GetColorData(this KinectSensor sensor, TimeSpan timeout)
+        {
+            if (sensor == null || !sensor.IsRunning) return null;
+
+            try
+            {
+                using (var frame = sensor.ColorStream.OpenNextFrame((int)timeout.TotalMilliseconds))
+                {
+                    if (frame == null) return null;
+
+                    return frame.GetRawPixelData();
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                // センサーが稼働していない、またはストリームが有効になっていないときにフレームを取得すると発生します。
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+
+        public static DepthImagePixel[] GetDepthData(this KinectSensor sensor, TimeSpan timeout)
+        {
+            if (sensor == null || !sensor.IsRunning) return null;
+
+            try
+            {
+                using (var frame = sensor.DepthStream.OpenNextFrame((int)timeout.TotalMilliseconds))
+                {
+                    if (frame == null) return null;
+
+                    return frame.GetRawPixelData();
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                // センサーが稼働していない、またはストリームが有効になっていないときにフレームを取得すると発生します。
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
