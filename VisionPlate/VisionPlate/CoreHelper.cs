dfd555200745923d96cdfcbfe62fa354dbe203fe
Keiho Sakapon <git@saka-pon.net>
2015-05-04 20:05:42 +0900

    Add helper class

---
+++ VisionPlate/VisionPlate/CoreHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VisionPlate
+{
+    public static class Enumerable2
+    {
+        public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+
+            foreach (var item in source)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+    }
+}
